82a5c36ae511010f6aa117dcca164e6dce3ae7b2
ADRIEN MARINI <adrien.marini@viacesi.fr>
2020-06-27 13:53:20 +0200

    Ajoutez des fichiers projet.

---
+++ PiConSetup/ClassFile.cs
@@ -0,0 +1,105 @@
+﻿/*
+ * Created by Adrien Marini - Copyright 2020
+ */
+
+using System;
+using System.IO;
+
+namespace PiConSetup
+{
+    /// <summary>
+    /// Classe permettant d'effectuer tous type de traitements génériques sur les fichiers
+    /// </summary>
+    public static class ClassFile
+    {
+        /// <summary>
+        /// Retourne le nom du lecteur selon les paramètres spécifiés
+        /// </summary>
+        /// <param name="volumeLabel"></param>
+        /// <param name="driveFormat"></param>
+        /// <returns></returns>
+        public static string GetNameDriveBoot(string volumeLabel, string driveFormat = null)
+        {
+            try
+            {
+                foreach (DriveInfo dr in DriveInfo.GetDrives())
+                {
+                    if (dr.IsReady && dr.VolumeLabel != "" && dr.VolumeLabel == volumeLabel)
+                    {
+                        if (driveFormat != null)
+                        {
+                            if (dr.DriveFormat == driveFormat)
+                            {
+                                return dr.Name;
+                            }
+                        }
+                        else
+                        {
+                            return dr.Name;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// Ecrit les lignes passées en paramètre dans le fichier type texte spécifié
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="lines"></param>
+        public static void WritingInTextFile(string path, string[] lines)
+        {
+            try
+            {
+                using (StreamWriter fileText = new StreamWriter(path))
+                {
+                    foreach (string l in lines)
+                    {
+                        fileText.WriteLine(l);
+                    }
+                }
+            }
+                        catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        /// <summary>
+        /// Controle que les lignes passées en paramètres correspondent parfaitement au contenu d'un fichier type texte.
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="lines"></param>
+        /// <returns></returns>
+        public static bool ControlTextFile(string path, string[] lines)
+        {
+            try
+            {
+                using (StreamReader fileText = new StreamReader(path))
+                {
+                    while (!fileText.EndOfStream)
+                    {
+                        foreach (string l in lines)
+                        {
+                            if (l != fileText.ReadLine())
+                            {
+                                return false;
+                            }
+                        }
+                    }
+                }
+                return true;
+            }
+                                    catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            return false;
+        }   
+    }
+}
---
+++ PiConSetup/Program.cs
@@ -0,0 +1,98 @@
+﻿/*
+ * Created by Adrien Marini - Copyright 2020
+ */
+
+
+using System;
+using System.IO;
+
+namespace PiConSetup
+{
+    class Program
+    {
+        private const string nameDriveBoot = "boot";
+        private const string nameDriveFormat = "FAT32";
+        private const string nameFileSSH = "ssh";
+        private const string nameFileWifi = "wpa_supplicant.conf";
+        private const string nameBoxWifi = "WifiBoxName";   //to replace
+        private const string passBoxWifi = "Password";  //to replace
+        private static string textFileWifi = "country=fr" + Environment.NewLine +
+                    "update_config=1" + Environment.NewLine +
+                    "ctrl_interface=/var/run/wpa_supplicant" + Environment.NewLine + Environment.NewLine +
+                    "network={" + Environment.NewLine +
+                    "scan_ssid=1" + Environment.NewLine +
+                    "ssid=\"" + nameBoxWifi + "\"" + Environment.NewLine +
+                    "psk=\"" + passBoxWifi + "\"" + Environment.NewLine +
+                    "}";
+
+        static void Main(string[] args)
+        {
+            string nameDrive = ClassFile.GetNameDriveBoot(nameDriveBoot, nameDriveFormat);
+            if (nameDrive != null)
+            {
+                Console.WriteLine("- Nom du lecteur boot = " + nameDrive);
+            }
+            else
+            {
+                Console.WriteLine("- Nom du lecteur boot n'as pas pu être récupéré.");
+            }
+
+            bool resultSSH = ActivateSSH(nameDrive + nameFileSSH);
+            if (resultSSH)
+            {
+                Console.WriteLine("- Fonction SSH activée avec succès.");
+            }
+            else
+            {
+                Console.WriteLine("- Fonction SSH n'a pas pu être activée correctement.");
+            }
+
+            bool resultWifi = ActivateWifi(nameDrive + nameFileWifi);
+            if (resultWifi)
+            {
+                Console.WriteLine("- Fonction Wifi activée avec succès.");
+            }
+            else
+            {
+                Console.WriteLine("- Fonction Wifi n'a pas pu être activée correctement.");
+            }
+
+            Console.WriteLine("Terminé.");
+            Console.ReadKey();
+        }
+
+        private static bool ActivateSSH(string path)
+        {
+            try
+            {
+                File.Create(path);
+                if (File.Exists(path)) return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            return false;
+        }
+
+        private static bool ActivateWifi(string path)
+        {
+            try
+            {
+                string[] text = textFileWifi.Split(Environment.NewLine);
+
+                ClassFile.WritingInTextFile(path, text);
+
+                if (File.Exists(path))
+                {
+                    return ClassFile.ControlTextFile(path, text);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            return false;
+        }
+    }
+}
